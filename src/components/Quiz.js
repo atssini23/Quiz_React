1a7043865d378e63c3520f0a69501cf490531bc8
Atssini Cabrera <atssini23@yahoo.com>
2020-12-23 11:50:16 -0800

    completed minimalist CSS

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -5,11 +5,14 @@ class Quiz extends React.Component {
   state = {
     userAnser: null,
     currentQuestion: 0,
-    option: []
+    options: [],
+    quizEnd: false,
+    score: 0,
+    disabled: true
   };
 
   loadQuiz = () => {
-    //console.log(quizData[0].question)
+    //console.log(QuizData[0].question)
     const { currentQuestion } = this.state;
     this.setState(() => {
       return {
@@ -23,12 +26,111 @@ class Quiz extends React.Component {
     this.loadQuiz();
   }
 
+  nextQuestionHandler = () => {
+    const { userAnswer, answer, score } = this.state;
+    this.setState({
+      currentQuestion: this.state.currentQuestion + 1
+    });
+    console.log(this.state.currentQuestion);
+    //increment the score if answer is correct
+    if (userAnswer === answer) {
+      this.setState({
+        score: score + 1
+      });
+    }
+  };
+  //updates the component
+  componentDidUpdate(prevProps, prevState) {
+    const { currentQuestion } = this.state;
+    if (this.state.currentQuestion !== prevState.currentQuestion) {
+      this.setState(() => {
+        return {
+          disabled: true,
+          questions: QuizData[currentQuestion].question,
+          options: QuizData[currentQuestion].options,
+          answer: QuizData[currentQuestion].answer
+        };
+      });
+    }
+  }
+  //check answer
+  checkAnswer = (answer) => {
+    this.setState({
+      userAnswer: answer,
+      disabled: false
+    });
+  };
+  finishHandler = () => {
+    const { userAnswer, answer, score } = this.state;
+
+    if (this.state.currentQuestion === QuizData.length - 1) {
+      this.setState({
+        quizEnd: true
+      });
+      if (userAnswer === answer) {
+        this.setState({
+          score: score + 1
+        });
+        console.log("Current score:", this.state.score);
+      }
+    }
+  };
+
   render() {
-    const { questions, options } = this.state;
+    const {
+      questions,
+      options,
+      currentQuestion,
+      userAnswer,
+      quizEnd
+    } = this.state;
+    if (quizEnd) {
+      return (
+        <div>
+          <h2>Your final score is {this.state.score} out of 4</h2>
+          <p>The correct answer's are: </p>
+          <ul>
+            {QuizData.map((item, index) => (
+              <li className="ui floating message options" key={index}>
+                {item.answer}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
-        {questions}
-        {options}
+        <h3>{questions}</h3>
+        <span>{` Question ${currentQuestion + 1} out of ${
+          QuizData.length
+        }`}</span>
+        {options.map((option) => (
+          <p
+            key={option.id}
+            className={`ui floating message options
+          ${userAnswer === option ? "selected" : null}
+          `}
+            onClick={() => this.checkAnswer(option)}
+          >
+            {option}
+          </p>
+        ))}
+        {currentQuestion < QuizData.length - 1 && (
+          <button
+            className="ui button black"
+            disabled={this.state.disabled}
+            onClick={this.nextQuestionHandler}
+          >
+            Next
+          </button>
+        )}
+        {currentQuestion === QuizData.length - 1 && (
+          <button className="ui button black" onClick={this.finishHandler}>
+            Finish
+          </button>
+        )}
       </div>
     );
   }
--- src/components/QuizData.js
+++ src/components/QuizData.js
@@ -2,25 +2,25 @@ export const QuizData = [
   {
     id: 0,
     question: ` What is the Capital Of India ?`,
-    options: [`New Delhi`, `Mumbai`, `Kolkatta`],
+    options: [`Mumbai`, `New Delhi`, `Kolkatta`],
     answer: `New Delhi`
   },
   {
     id: 1,
     question: `Who is the CEO of Tesla Motors?`,
-    options: [`Bill Gates`, `Steve Jobs`, `Elon Musk`],
+    options: [`Elon Musk`, `Steve Jobs`, `Bill Gates`],
     answer: `Elon Musk`
   },
   {
-    id: 3,
+    id: 2,
     question: `Name World's Richest Man?`,
-    options: [`Jeff Bezo`, `Bill Gates`, `Mark Zuckerberg`],
+    options: [`Bill Gates`, `Mark Zuckerberg`, `Jeff Bezo`],
     answer: `Jeff Bezo`
   },
   {
-    id: 4,
+    id: 3,
     question: `World's Longest River?`,
-    options: [`River Nile`, `River Amazon`, `River Godavari`],
+    options: [`River Godavari`, `River Nile`, `River Amazon`],
     answer: `River Nile`
   }
 ];
